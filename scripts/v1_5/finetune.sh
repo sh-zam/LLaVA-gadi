8871415319b1ba38b5dbc3afa761fd07258cbe3a
Sharaf Zaman <shzam@sdf.org>
2023-12-06 20:34:04 +1100

    Commit some changes for inference

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -2,13 +2,13 @@
 
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3_offload.json \
+    --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
     --data_path /scratch/dg97/sz7583/data-sources/datasets/apod/apod_conversations.json \
     --image_folder /scratch/dg97/sz7583/data-sources/datasets/apod/ \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/astrollava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -16,11 +16,11 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b \
+    --output_dir ./checkpoints/astrollava-v1.5-7b-lora \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 5000 \
@@ -32,7 +32,43 @@ deepspeed llava/train/train_mem.py \
     --logging_steps 1 \
     --tf32 True \
     --model_max_length 2048 \
-    --gradient_checkpoint True \
-    --dataloader_num_workers 1 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to none
+
+# deepspeed llava/train/train_mem.py \
+#     --deepspeed ./scripts/zero3.json \
+#     --model_name_or_path liuhaotian/llava-v1.5-7b \
+#     --version v1 \
+#     --data_path /scratch/dg97/sz7583/data-sources/datasets/apod/apod_conversations.json \
+#     --image_folder /scratch/dg97/sz7583/data-sources/datasets/apod/ \
+#     --vision_tower openai/clip-vit-large-patch14-336 \
+#     --pretrain_mm_mlp_adapter ./checkpoints/astrollava-v1.5-7b-pretrain/mm_projector.bin \
+#     --mm_projector_type mlp2x_gelu \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --image_aspect_ratio pad \
+#     --group_by_modality_length True \
+#     --bf16 True \
+#     --output_dir ./checkpoints/astrollava-v1.5-7b \
+#     --num_train_epochs 1 \
+#     --per_device_train_batch_size 16 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 5000 \
+#     --save_total_limit 1 \
+#     --learning_rate 2e-5 \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 True \
+#     --model_max_length 2048 \
+#     --gradient_checkpointing True \
+#     --dataloader_num_workers 4 \
+#     --lazy_preprocess True \
+#     --report_to none
