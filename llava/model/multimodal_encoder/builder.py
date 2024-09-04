4c5b9a5101cadb933c7df188a11bc68e7918cd9d
Sharaf Zaman <shzam@sdf.org>
2024-09-04 21:47:58 +1000

    Conver the input vision tower to bfloat16

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -8,8 +8,12 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     use_s2 = getattr(vision_tower_cfg, 's2', False)
     if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
         if use_s2:
-            return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+            model = CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+            print('Converting to bfloat16')
+            return model.bfloat16()
         else:
-            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+            model = CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+            print('Converting to bfloat16')
+            return model.bfloat16()
 
     raise ValueError(f'Unknown vision tower: {vision_tower}')
