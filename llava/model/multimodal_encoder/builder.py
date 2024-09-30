4488e883caca32baa482f57df8363ece8df9dd94
Sharaf Zaman <shzam@sdf.org>
2024-09-30 19:14:52 +1000

    Revert "Conver the input vision tower to bfloat16"
    This reverts commit 4c5b9a5101cadb933c7df188a11bc68e7918cd9d.

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -8,12 +8,8 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     use_s2 = getattr(vision_tower_cfg, 's2', False)
     if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
         if use_s2:
-            model = CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
-            print('Converting to bfloat16')
-            return model.bfloat16()
+            return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
         else:
-            model = CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-            print('Converting to bfloat16')
-            return model.bfloat16()
+            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
 
     raise ValueError(f'Unknown vision tower: {vision_tower}')
